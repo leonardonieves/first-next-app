966f1e6b24a71571ab55514356b3414dde446ab2
Leonardo Nieves <leonardo.nieves@ntsprint.com>
2023-08-29 06:35:29 -0400

    Add MUI styles

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,27 +1,47 @@
-'use client'
+'use client';
 
-// app/note/Page.tsx
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import {
+  Typography,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
+import { Delete, Edit, Check, Close } from '@mui/icons-material';
 
 function NotePage() {
   const [notes, setNotes] = useState<string[]>([]);
   const [editNoteIndex, setEditNoteIndex] = useState<number | null>(null);
   const [editedNote, setEditedNote] = useState('');
+  const [openDialog, setOpenDialog] = useState(false);
+  const [deleteNoteIndex, setDeleteNoteIndex] = useState<number | null>(null);
 
   const validationSchema = Yup.object().shape({
     newNote: Yup.string().required('The field is required'),
   });
 
-  const addNote = (values: { newNote: string; }, actions: { resetForm: () => void; }) => {
+  const addNote = (values: { newNote: string; }) => {
     setNotes([...notes, values.newNote]);
-    actions.resetForm();
+    setEditedNote(''); // Clear the edited note after adding
   };
 
-  const deleteNote = (index: number) => {
+  const deleteNote = (index: number | null) => {
+    console.log('aqui');
     const updatedNotes = notes.filter((_, i) => i !== index);
     setNotes(updatedNotes);
+    setEditNoteIndex(null);
+    setDeleteNoteIndex(null);
+    closeDeleteConfirmation();
   };
 
   const editNote = (index: number) => {
@@ -41,60 +61,138 @@ function NotePage() {
     }
   };
 
+  const openDeleteConfirmation = (index: number) => {
+    setOpenDialog(true);
+    setDeleteNoteIndex(index);
+  };
+
+  const closeDeleteConfirmation = () => {
+    setOpenDialog(false);
+    setDeleteNoteIndex(null);
+  };
+
   return (
     <div>
-      <h1>Lista de Notas</h1>
+      <Typography variant="h4">Lista de Notas</Typography>
 
       <Formik
         initialValues={{ newNote: '' }}
         validationSchema={validationSchema}
         onSubmit={(values, actions) => {
           if (values.newNote.trim() !== '') {
-            addNote(values, actions);
+            addNote(values);
+            actions.resetForm();
           }
         }}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field
+        {({ isSubmitting, values, touched, errors, handleChange, handleBlur, handleSubmit }) => (
+          <form
+            className='w-full'
+            noValidate
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <TextField
               type="text"
               name="newNote"
-              placeholder="New Nota"
+              label="Nueva Nota"
+              variant="outlined"
+              fullWidth
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.newNote}
+              error={touched.newNote && Boolean(errors.newNote)}
+              helperText={touched.newNote && errors.newNote}
             />
-            <ErrorMessage name="newNote" component="div" />
 
-            <button type="submit" disabled={isSubmitting}>Agregar</button>
-          </Form>
+
+            <Button type="submit" variant="contained" color="primary">
+              Add Note
+            </Button>
+          </form>
         )}
       </Formik>
 
-      <ul>
+      <List>
         {notes.map((note, index) => (
-          <li key={index}>
+          <ListItem key={index}>
             {index === editNoteIndex ? (
               <>
-                <Field
-                  type="text"
-                  name="editedNote"
-                  value={editedNote}
-                  onChange={(e: { target: { value: SetStateAction<string>; }; }) => setEditedNote(e.target.value)}
-                />
-                <button onClick={saveEditedNote}>Save</button>
+                <Formik
+                  initialValues={{ editedNote }}
+                  onSubmit={saveEditedNote}
+                >
+                  {({ isSubmitting }) => (
+                    <Form>
+                      <TextField
+                        type="text"
+                        name="editedNote"
+                        label="Editar Nota"
+                        variant="outlined"
+                        value={editedNote}
+                        onChange={(e) => setEditedNote(e.target.value)}
+                        fullWidth
+                      />
+                      <ErrorMessage name="editedNote" component="div" className="error-message" />
+                      <Button
+                        type="submit"
+                        disabled={isSubmitting}
+                        variant="contained"
+                        color="primary"
+                        startIcon={<Check />}
+                      >
+                        Guardar
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        color="secondary"
+                        onClick={() => setEditNoteIndex(null)}
+                        startIcon={<Close />}
+                      >
+                        Cancelar
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
               </>
             ) : (
               <>
-                {note}
-                <button onClick={() => editNote(index)}>Edit</button>
-                <button onClick={() => {
-                  if (window.confirm("¿Estás seguro de que deseas eliminar esta nota?")) {
-                    deleteNote(index);
-                  }
-                }}>Delete</button>
+                <ListItemText primary={note} />
+                <ListItemSecondaryAction>
+                  <IconButton onClick={() => editNote(index)} color="primary">
+                    <Edit />
+                  </IconButton>
+                  <IconButton onClick={() => openDeleteConfirmation(index)} color="secondary">
+                    <Delete />
+                  </IconButton>
+                </ListItemSecondaryAction>
               </>
             )}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
+
+      <Dialog open={openDialog} onClose={closeDeleteConfirmation}>
+        <DialogTitle>Confirmar Eliminación</DialogTitle>
+        <DialogContent>
+          ¿Estás seguro de que deseas eliminar esta nota?
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDeleteConfirmation} color="primary">
+            Cancelar
+          </Button>
+          <Button
+            onClick={() => {
+              deleteNote(deleteNoteIndex);
+            }}
+            color="secondary"
+          >
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link';
+import { Button, Typography } from '@mui/material';
 
 export default function Page() {
-  return ( <div>
-    <h1>Home page</h1>
-    <Link href="/note">Notes</Link>
-  </div>);
+  return (
+    <div>
+      <Typography variant="h2">Dashboard</Typography>
+      <Button component={Link} href="/note" variant="contained" color="primary">
+        Go to Notes
+      </Button>
+    </div>
+  );
 }
